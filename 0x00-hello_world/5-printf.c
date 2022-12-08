83841b647c92b5c302c66f37d75e5c68dee1bd31
Sagastein <ishimwe.sage.3@gmail.com>
2022-12-08 09:35:58 -0800

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints "with proper grammar, but the outcome is a piece of art,"
- *   *
- *    * Return: Always 0 (Success)
+ *main - Prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ *Return: Always 0 (Success)
  *     */
 int main(void)
 {
