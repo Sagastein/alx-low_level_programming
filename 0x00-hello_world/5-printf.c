59ef41d1948dd5bc0d51315ecc6cbbd9d47adcf2
Sagastein <72102004+Sagastein@users.noreply.github.com>
2022-12-08 19:50:50 +0200

    Update 5-printf.c
    adding hard

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
-* main - Prints "with proper grammar, but the outcome is a piece of art,"
-*
-*Return: Always 0 (Success)   
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
