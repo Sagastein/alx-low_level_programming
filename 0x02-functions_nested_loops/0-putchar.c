d29dd8a23b5fe1bfdbf69a70a14b53689ddc7abe
Sagastein <ishimwe.sage.3@gmail.com>
2022-12-13 08:10:34 -0800

    heading

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
 #include "main.h"
+
+
 /**
- * main - calls _putchar to print str
+ * main - Entry point
  *
- * Return: value 0
- *
+ * Return: 0 if successful.
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
