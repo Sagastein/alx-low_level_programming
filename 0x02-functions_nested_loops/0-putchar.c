bfc032dd542bd919c917fed9e626be2bb258f508
Sagastein <ishimwe.sage.3@gmail.com>
2022-12-13 08:04:24 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
-
 /**
- * main - Print _putchar
- * Return: Always 0
+ * main - calls _putchar to print str
+ *
+ * Return: value 0
+ *
  */
+
 int main(void)
 {
 	_putchar('_');
