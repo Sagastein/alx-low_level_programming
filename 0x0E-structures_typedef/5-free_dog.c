5ae3b984005b50d685860c8c03ddb5eef63d3dbe
Sagastein <ishimwe.sage.3@gmail.com>
2023-01-17 05:17:34 -0800

    laster

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
-/**
- * main - check the code
- * Return: Always 0.
- */
-int main(void)
-{
-	dog_t *my_dog;
 
-	my_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-	free_dog(my_dog);
-	return (0);
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
