ad9350908b2c5ebb1e9fcf22c8934d88deec772c
Sagastein <ishimwe.sage.3@gmail.com>
2023-01-30 08:21:15 -0800

    linked list in c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -8,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX SE project
  */
 typedef struct list_s
 {
